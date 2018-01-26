96a253114fd1c9c773ede8f9d540451ebdd9bd9c
root <root@localhost.localdomain>
2018-01-26 18:00:28 +0800

    modify

--- week5/football_team_europe.py
+++ week5/football_team_europe.py
@@ -4,6 +4,7 @@ import csv
 import pandas as pd
 import re
 import math
+import traceback
 from week5.consts import LEAGUE_MAP
 from week5.util import write_csv
 
@@ -21,13 +22,19 @@ def percent_to_float(df, pattern):
 
 def _analyze_team_data(teams, match_index):
     url_match = 'https://g.hupu.com/soccer/data_{}.html'.format(match_index)
-    data = pd.read_html(url_match)
+    try:
+        data = pd.read_html(url_match)
+    except Exception as e:
+        print(e)
+        print(traceback.format_exc())
+        return None
     if len(data) < 4:  # match not completed
-        return
+        return None
     teamname_home = data[2][0][0]
     teamname_away = data[2][2][0]
-    teams['home'][teamname_home] = _parse_team_total(data, teams, teamname_home, 6)
-    teams['away'][teamname_away] = _parse_team_total(data, teams, teamname_away, 7)
+    teams['home'][teamname_home], col_names = _parse_team_total(data, teams, teamname_home, 6)
+    teams['away'][teamname_away], _ = _parse_team_total(data, teams, teamname_away, 7)
+    return col_names
 
 
 def _parse_team_total(data, teams, teamname, index):
@@ -37,7 +44,7 @@ def _parse_team_total(data, teams, teamname, index):
     col_names = data[index][:1].values.tolist()[0]
     subtotal = data[index][(rows - 1):]
     team_data = _parse_team_data(teams, teamname, col_names, subtotal)
-    return team_data
+    return team_data, col_names
 
 
 def _parse_team_data(teams, teamname_home, col_names, subtotal):
@@ -53,23 +60,38 @@ def _parse_team_data(teams, teamname_home, col_names, subtotal):
     return data_home
 
 
-for league in LEAGUE_MAP:
-    if league != 'germany':
-        continue
+def _process_team_data(teams, league):
+    match_filename = 'match_{}.csv'.format(league)
+    csv_reader = csv.reader(open(match_filename, encoding='utf-8'))
+    col_names = None
+    count = 0
+    for row in csv_reader:
+        if count > 0:
+            break
+        col_names = _analyze_team_data(teams, int(row[0]))
+        if col_names:
+            count = count + 1
+    # 数据写入csv文件
+    if teams['home']:
+        write_csv('teams_{}_home.csv'.format(league), teams['home'], columns=col_names)
+    if teams['away']:
+        write_csv('teams_{}_away.csv'.format(league), teams['away'], columns=col_names)
+    if teams['subtotal']:
+        write_csv('teams_{}_subtotal.csv'.format(league), teams['subtotal'], columns=col_names)
+
+
+if __name__ == '__main__':
     teams = {
         'home': defaultdict(dict),
         'away': defaultdict(dict),
         'subtotal': defaultdict(dict),
         'matches': defaultdict(dict),
     }
-    match_filename = 'match_{}.csv'.format(league)
-    csv_reader = csv.reader(open(match_filename, encoding='utf-8'))
-    for row in csv_reader:
-        _analyze_team_data(teams, int(row[0]))
-    # 数据写入csv文件
-    if teams['home']:
-        write_csv('teams_{}_home.csv'.format(league), teams['home'])
-    if teams['away']:
-        write_csv('teams_{}_away.csv'.format(league), teams['away'])
-    if teams['subtotal']:
-        write_csv('teams_{}_subtotal.csv'.format(league), teams['subtotal'])
+    # for league in LEAGUE_MAP:
+    #     if league != 'germany':
+    #         continue
+    #     _process_team_data(league)
+    i = 10854899
+    col_names = _analyze_team_data(teams, i)
+    if col_names:
+        print(teams)
--- week5/util.py
+++ week5/util.py
@@ -2,11 +2,17 @@ import csv
 from collections import Iterable
 
 
-def write_csv(filename, content, header=None):
+def write_csv(filename, content, header=None, columns=None):
     file = open(filename, "w")
     csvwriter = csv.writer(file, dialect=("excel"))
     if header:
         csvwriter.csvwriterow(header)
     for row in content:
-        encoderow = list(row) if isinstance(row, Iterable) and not isinstance(row, str) else [row, ]
+        if isinstance(row, Iterable):
+            if isinstance(row, dict):
+                encoderow = [row[col] for col in columns] if columns else row.values
+            elif not isinstance(row, str):
+                encoderow = list(row)
+            else:
+                encoderow = [row, ]
         csvwriter.writerow(encoderow)
